8498d478895a8a28de37ed9aaf8dfd2575dad105
asarda22 <akshayscareers@gmail.com>
2022-03-01 21:13:06 +0200

    Profile update screen

---
+++ app/src/main/java/com/example/profileupdate/MainActivity.kt
@@ -0,0 +1,184 @@
+package com.example.profileupdate
+
+import android.net.Uri
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
+import com.example.profileupdate.ui.theme.ProfileUpdateTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ProfileUpdateTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    ProfileUpdate()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ProfileUpdate() {
+
+    val notificationMessage = rememberSaveable{ mutableStateOf("") }
+    if (notificationMessage.value.isNotEmpty()) {
+        Toast.makeText(LocalContext.current, notificationMessage.value, Toast.LENGTH_LONG).show()
+        notificationMessage.value = ""
+    }
+
+    var name by rememberSaveable { mutableStateOf("name") }
+    var username by rememberSaveable { mutableStateOf("username") }
+    var aboutme by rememberSaveable { mutableStateOf("about me") }
+
+    Column(
+        modifier = Modifier
+            .verticalScroll(rememberScrollState())
+            .padding(8.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(text = "Cancel",
+                modifier = Modifier.clickable {
+                    name = ""
+                    username = ""
+                    aboutme = ""
+
+                    notificationMessage.value = "Cancelled"
+                })
+            Text(text = "Save",
+                modifier = Modifier.clickable { notificationMessage.value = "Profile updated" })
+        }
+
+        ProfileImage()
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 4.dp, end = 4.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(text = "Name", modifier = Modifier.width(100.dp))
+            TextField(
+                value = name,
+                onValueChange = { name = it },
+                colors = TextFieldDefaults.textFieldColors(
+                    backgroundColor = Color.Transparent,
+                    textColor = Color.Black
+                )
+            )
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 4.dp, end = 4.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(text = "Username", modifier = Modifier.width(100.dp))
+            TextField(
+                value = username,
+                onValueChange = { username = it },
+                colors = TextFieldDefaults.textFieldColors(
+                    backgroundColor = Color.Transparent,
+                    textColor = Color.Black
+                )
+            )
+        }
+
+        Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        verticalAlignment = Alignment.Top
+    ) {
+        Text(
+            text = "About me", modifier = Modifier
+                .width(100.dp)
+                .padding(top = 8.dp)
+        )
+        TextField(
+            value = aboutme,
+            onValueChange = { aboutme = it },
+            colors = TextFieldDefaults.textFieldColors(
+                backgroundColor = Color.Transparent,
+                textColor = Color.Black
+            ),
+            singleLine = false,
+            modifier = Modifier.height(100.dp)
+        )
+    }
+    }
+}
+
+@Composable
+fun ProfileImage() {
+    val imageUri = rememberSaveable { mutableStateOf("") }
+    val painter = rememberImagePainter(
+        if (imageUri.value.isEmpty())
+            R.drawable.ic_baseline_person_24
+        else
+            imageUri.value
+    )
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetContent()
+    ) { uri: Uri? ->
+        uri?.let { imageUri.value = it.toString() }
+    }
+
+    Column(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Card(
+            shape = CircleShape,
+            modifier = Modifier
+                .padding(8.dp)
+                .size(100.dp)
+        ) {
+            Image(
+                painter = painter,
+                contentDescription = null,
+                modifier = Modifier
+                    .wrapContentSize()
+                    .clickable {launcher.launch("image/*") },
+                contentScale = ContentScale.Crop
+            )
+        }
+        Text(text = "Change profile picture")
+    }
+}
